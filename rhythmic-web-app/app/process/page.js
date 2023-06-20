16bb15aa42ede1ef97cc66c3a0fc46d249ff65f6
unknown <michael.chian9@gmail.com>
2023-06-19 19:09:41 -0700

    start linking web app to docker

--- rhythmic-web-app/app/process/page.js
+++ rhythmic-web-app/app/process/page.js
@@ -7,11 +7,11 @@ export default function Process() {
         <h1 className={styles.titleTag}>Start Comparing Your Music</h1>
         <form action="/api/upload" method="post" name="sheet-music-upload" encType="multipart/form-data">
           <div>
-            <input name="sheet-music-img" type="file" accept="image/png, image/jpg" required />
-          </div>
-          <div>
             <label htmlFor="sheet-music-name">Name of Sheet Music</label>
             <input id="sheet-music-name" name="sheet-music-name" type="text" required />
+          </div>
+          <div>
+            <input name="sheet-music-img" type="file" accept="image/png, image/jpg" required />
           </div>
           <button type="submit">Upload</button>
         </form>
--- rhythmic-web-app/pages/api/upload.js
+++ rhythmic-web-app/pages/api/upload.js
@@ -19,29 +19,45 @@ export const config = {
 async function upload(req) {
     return new Promise((resolve, reject) => {
         const bb = busboy({ headers: req.headers });
+        let filePath = '';
+        let projectName = '';
         bb.on('file', (name, file, info) => {
-            const saveTo = path.join(os.tmpdir(), `${name}-${random()}`);
+            const saveTo = path.join(`${__dirname}/../../../../../testvolume/`, `${name}-${random()}`);
             console.log('File [' + name + ']: filename: ' + info.filename, saveTo);
             file.pipe(fs.createWriteStream(saveTo));
+            filePath = saveTo;
         });
-        bb.on('text', (name, text, info) => {
-
+        bb.on('field', (fieldName, value) => {
+            if(fieldName == 'sheet-music-name') {
+                projectName = value;
+            }
         });
         bb.on('finish', () => {
             console.log('finished');
-            resolve();
+            resolve({
+                projectName, 
+                filePath
+            });
         });
         bb.on('error', reject);
         req.pipe(bb);
     });
 }
 
+function sendJSONToDocker(jsonObject) {
+    console.log(__dirname);
+    // fs.writeFile('sheet-music-pos.json', jsonObject);
+    console.log('wrote to somehwere');
+}
+
 export default async function handler(req, res) {
     if (req.method == 'POST') {
         console.log('POST');
         try {
-            await upload(req);
-            res.status(200).json({ upload: 'Success' });
+            const jsonObject = await upload(req);
+            sendJSONToDocker(jsonObject);
+            res.status(200).json('success');
+            
         } catch (e) {
             res.status(500).json({ error: e.message });
         }
