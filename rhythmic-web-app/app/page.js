097a354484046fe03c4eaef4d3f902d96a91a7b5
unknown <michael.chian9@gmail.com>
2023-06-30 23:56:34 -0700

    added style with material ui

--- rhythmic-web-app/app/page.js
+++ rhythmic-web-app/app/page.js
@@ -1,12 +1,16 @@
+'use client'
+
 import Image from 'next/image'
 import styles from './page.module.css'
 import Link from 'next/link'
+
+import { Button } from '@mui/material'
 
 export default function Home() {
   return (
     <main className={styles.main}>
       <div className={styles.centerButtons}>
-        <Link href="/project" className={styles.button}>Get started with a new project</Link>
+        <Button href="/project" className={styles.button}>Get started with a new project</Button>
       </div>
     </main>
   )
--- rhythmic-web-app/app/project/page.js
+++ rhythmic-web-app/app/project/page.js
@@ -1,22 +1,65 @@
+'use client'
+
 import styles from '../page.module.css'
 import Link from 'next/link'
 
+import { Button, IconButton, InputLabel, TextField, Unstable_Grid2 as Grid, Typography } from '@mui/material'
+import { Add as AddIcon } from '@mui/icons-material';
+import { useState, useRef } from "react";
+
 export default function Process() {
+    const [previewImageUrl, setPreviewImageUrl] = useState(null);
+    const ref = useRef();
+
+    const handleFileUpload = (event) => {
+        // when the uploaded file is chosen,
+        // convert it to a data URL, and set it as the value of the preview image
+        const file = event.target.files[0];
+        const reader = new FileReader();
+
+        reader.addEventListener('load', () => {
+            setPreviewImageUrl(reader.result);
+        });
+
+        reader.readAsDataURL(file);
+    };
+
     return (
       <main className={styles.main}>
-        <h1 className={styles.titleTag}>Start Comparing Your Music</h1>
+        <Typography variant="h3" component="h1" align="left" gutterBottom>
+            Create a new project
+        </Typography>
         <form action="/api/upload" method="post" name="sheet-music-upload" encType="multipart/form-data">
-          <div>
-            <label htmlFor="sheet-music-name">Name of Sheet Music</label>
-            <input id="sheet-music-name" name="sheet-music-name" type="text" required />
-          </div>
-          <div>
-            <label htmlFor="sheet-music-img">Add Images of Sheet Music in Order</label>
-            <input name="sheet-music-img" type="file" accept="image/png, image/jpg, image/jpeg" multiple required />
-          </div>
-          <button type="submit">Upload</button>
-        </form>
-        <Link href="/">Return to Home</Link>
+            <Grid container spacing={1}>
+                <Grid xs={4}>
+                    <InputLabel htmlFor="sheet-music-name">Project Name</InputLabel>
+                </Grid>
+                <Grid xs={8}>
+                    <TextField id="sheet-music-name" name="sheet-music-name" variant="outlined" size="small" required />
+            </Grid>
+            <Grid xs={4}>
+                <InputLabel htmlFor="sheet-music-name">Sheet Music Images</InputLabel>
+            </Grid>
+            <Grid xs={8}>
+                {previewImageUrl && <img src={previewImageUrl} alt="Uploaded Image" height="100" style={{'border-radius':'5px'}}/>}
+                <IconButton color="primary" aria-label="upload picture"
+                    onClick={(e) => ref.current?.click()}
+                    variant="contained" component="span"
+                    >
+                      <AddIcon />
+                </IconButton>
+                <input name="sheet-music-img" type="file" accept="image/png, image/jpg, image/jpeg"
+                    style={{display: 'none'}} onChange={handleFileUpload} required
+                    ref={ref}
+                    />
+            </Grid>
+            <Grid xs={4}>
+                <Button variant="contained" type="submit">
+                    Create Project
+                </Button>
+            </Grid>
+            </Grid>
+          </form>
       </main>
     )
   }
